a4f9411b78c4e9df49f77e18e2d4c5a465eb4a1d
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-06-04 14:02:56 +0000

    finished smart contract & testing

---
+++ packages/hardhat/contracts/Social.sol
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Social {
+    // Address of the contract owner
+    address public owner;
+
+    // Struct representing a user
+    struct User {
+        string username;
+        address userAddress;
+        bool isRegistered;
+    }
+
+    // Mapping from user address to user details
+    mapping(address => User) public users;
+
+    // Struct representing a post
+    struct Post {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+        uint256 commentsCount;
+    }
+
+    // Struct representing a comment
+    struct Comment {
+        address commenter;
+        string content;
+        uint256 timestamp;
+    }
+
+    // Mapping from post ID to comment ID to comment details
+    mapping(uint256 => mapping(uint256 => Comment)) public postComments;
+
+    // Mapping from post ID to count of comments
+    mapping(uint256 => uint256) public postCommentsCount;
+
+    // Array to store all posts
+    Post[] public posts;
+
+    // Event emitted when a user is registered
+    event UserRegistered(address indexed userAddress, string username);
+
+    // Event emitted when a post is created
+    event PostCreated(address indexed author, string content, uint256 timestamp);
+
+    // Event emitted when a post is liked
+    event PostLiked(address indexed liker, uint256 indexed postId);
+
+    // Event emitted when a comment is added
+    event CommentAdded(address indexed commenter, uint256 indexed postId, string content, uint256 timestamp);
+
+    // Modifier to restrict function access to registered users
+    modifier onlyRegisteredUser() {
+        require(users[msg.sender].isRegistered, "User is not registered");
+        _;
+    }
+
+    // Modifier to restrict function access to the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    // Constructor to set the owner of the contract to the deployer
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Function to register a new user
+    function registerUser(string memory _username) external {
+        require(!users[msg.sender].isRegistered, "User is already registered");
+        require(bytes(_username).length > 0, "Username should not be empty");
+
+        users[msg.sender] = User({
+            username: _username,
+            userAddress: msg.sender,
+            isRegistered: true
+        });
+
+        emit UserRegistered(msg.sender, _username);
+    }
+
+    // Function to get user details by address
+    function getUserByAddress(address _userAddress) external view returns (User memory) {
+        require(users[_userAddress].isRegistered, "User not found");
+        return users[_userAddress];
+    }
+
+    // Function to create a new post
+    function createPost(string memory _content) external onlyRegisteredUser {
+        require(bytes(_content).length > 0, "Content should not be empty");
+
+        posts.push(Post({
+            author: msg.sender,
+            content: _content,
+            timestamp: block.timestamp,
+            likes: 0,
+            commentsCount: 0
+        }));
+
+        emit PostCreated(msg.sender, _content, block.timestamp);
+    }
+
+    // Function to like a post
+    function likePost(uint256 _postId) external onlyRegisteredUser {
+        require(_postId < posts.length, "Post does not exist");
+
+        Post storage post = posts[_postId];
+        post.likes++;
+
+        emit PostLiked(msg.sender, _postId);
+    }
+
+    // Function to add a comment to a post
+    function addComment(uint256 _postId, string memory _content) external onlyRegisteredUser {
+        require(_postId < posts.length, "Post does not exist");
+        require(bytes(_content).length > 0, "Comment should not be empty");
+
+        uint256 commentId = postCommentsCount[_postId];
+        postComments[_postId][commentId] = Comment({
+            commenter: msg.sender,
+            content: _content,
+            timestamp: block.timestamp
+        });
+
+        postCommentsCount[_postId]++;
+        posts[_postId].commentsCount++;
+
+        emit CommentAdded(msg.sender, _postId, _content, block.timestamp);
+    }
+
+    // Function to get the count of posts
+    function getPostsCount() external view returns (uint256) {
+        return posts.length;
+    }
+
+    // Function to get details of a specific post
+    function getPost(uint256 _postId) external view returns (
+        address author,
+        string memory content,
+        uint256 timestamp,
+        uint256 likes,
+        uint256 commentsCount
+    ) {
+        require(_postId < posts.length, "Post does not exist");
+        Post memory post = posts[_postId];
+        return (post.author, post.content, post.timestamp, post.likes, post.commentsCount);
+    }
+
+    // Function to get details of a specific comment on a post
+    function getComment(uint256 _postId, uint256 _commentId) external view returns (
+        address commenter,
+        string memory content,
+        uint256 timestamp
+    ) {
+        require(_postId < posts.length, "Post does not exist");
+        require(_commentId < postCommentsCount[_postId], "Comment does not exist");
+
+        Comment memory comment = postComments[_postId][_commentId];
+        return (comment.commenter, comment.content, comment.timestamp);
+    }
+}
--- packages/hardhat/test/YourContract.ts
+++ packages/hardhat/test/YourContract.ts
@@ -1,28 +1,70 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { YourContract } from "../typechain-types";
+import { Social } from "../typechain-types";
 
-describe("YourContract", function () {
+describe("Social", function () {
   // We define a fixture to reuse the same setup in every test.
 
-  let yourContract: YourContract;
+  let social: Social;
+  let user1: any;
+  let user2: any;
+
   before(async () => {
-    const [owner] = await ethers.getSigners();
-    const yourContractFactory = await ethers.getContractFactory("YourContract");
-    yourContract = (await yourContractFactory.deploy(owner.address)) as YourContract;
-    await yourContract.waitForDeployment();
+    [user1, user2] = await ethers.getSigners();
+    const socialFactory = await ethers.getContractFactory("Social");
+    social = (await socialFactory.deploy()) as Social;
+    await social.waitForDeployment();
   });
 
-  describe("Deployment", function () {
-    it("Should have the right message on deploy", async function () {
-      expect(await yourContract.greeting()).to.equal("Building Unstoppable Apps!!!");
-    });
+  it("should register a user", async function () {
+    const username = "user1";
 
-    it("Should allow setting a new message", async function () {
-      const newGreeting = "Learn Scaffold-ETH 2! :)";
+    await social.connect(user1).registerUser(username);
+    const user = await social.users(user1.address);
 
-      await yourContract.setGreeting(newGreeting);
-      expect(await yourContract.greeting()).to.equal(newGreeting);
-    });
+    expect(user.username).to.equal(username);
+    expect(user.userAddress).to.equal(user1.address);
+    expect(user.isRegistered).to.be.true;
+  });
+
+  it("should create a post", async function () {
+    const content = "This is a test post";
+    await social.connect(user1).createPost(content);
+    const postsCount = await social.getPostsCount();
+    expect(postsCount.toString()).to.equal("1");
+
+    const post = await social.getPost(0);
+    expect(post.author).to.equal(user1.address);
+    expect(post.content).to.equal(content);
+    expect(post.likes.toString()).to.equal("0");
+    expect(post.commentsCount.toString()).to.equal("0");
+  });
+
+  it("should like a post", async function () {
+    await social.connect(user2).registerUser("user2");
+    await social.connect(user2).likePost(0);
+    const post = await social.getPost(0);
+    expect(post.likes.toString()).to.equal("1");
+    expect(post.author).to.equal(user1.address);
+  });
+
+  it("should add a comment to a post", async function () {
+    // const content = 'This is a test post';
+    const commentContent = "This is a test comment";
+
+    await social.connect(user2).addComment(0, commentContent);
+
+    const comment = await social.getComment(0, 0);
+    expect(comment.commenter).to.equal(user2.address);
+    expect(comment.content).to.equal(commentContent);
+
+    const post = await social.getPost(0);
+    expect(post.commentsCount.toString()).to.equal("1");
+  });
+
+  it("should return correct user details by address", async function () {
+    const userDetails = await social.getUserByAddress(user1.address);
+    expect(userDetails.userAddress).to.equal(user1.address);
+    expect(userDetails.isRegistered).to.equal(true);
   });
 });
